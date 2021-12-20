ca1ddc834977ff2ef766971af80fe23b207f303c
Jake Salgado <5466068+jaek-s@users.noreply.github.com>
2021-12-20 11:29:17 -0700

    add a colors file

---
+++ src/colors.ts
@@ -0,0 +1,11 @@
+export default {
+  white: "rgb(255, 255, 255)",
+  lightGrey: "rgb(248 ,248, 248)",
+  mediumGrey: "rgb(171, 175, 177)",
+  darkGrey: "rgb(109, 109, 109)",
+  black: "rgb(0, 0, 0)",
+  extraLightBlue: "rgb(236, 245, 249)",
+  lightBlue: "rgb(165, 202, 217)",
+  mediumBlue: "rgb(0, 148, 184)",
+  darkBlue: "rgb(6, 34, 44)",
+};
