b3d96a8c4a4d17f19059cffaa74c14bf223c8f38
Daren Keck <daren.keck@devetry.com>
2022-01-19 10:37:27 -0800

    Add 'jsontomodelica' and 'modelicatojson' API placeholders

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import * as parser from "./../dependencies/modelica-json/lib/parser";
+import { json } from 'stream/consumers';
 
 const app = express();
 const PORT = 3000; // TODO: move to ENV file to share with docker-compose
@@ -14,11 +15,16 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/jsontomodelica', async (req, res) => {
-    const jsonToConvert = req.body;
+    // const jsonToConvert = req.body;
+    // // TODO: a better place to store converted modelica needs to be found.
+    // const modelica = parser.convertToModelica(jsonToConvert, "./modelica");
 
-    // TODO: a better place to store converted modelica needs to be found.
-    const modelica = parser.convertToModelica(jsonToConvert, "./modelica");
-    // TODO call parser directly
+    res.send("TODO: convert json to modelica");
+});
+
+app.post('/api/modelicatojson', async (req, res) => {
+    // TODO
+    res.send("TODO: convert modelica to JSON")
 });
 
 app.listen(PORT, () => {
