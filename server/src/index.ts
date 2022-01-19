41558147da4f2b35cf8220acc8a72087e6727887
Daren Keck <daren.keck@devetry.com>
2022-01-19 14:43:34 -0800

    Fix missed rebase update

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,19 +3,15 @@ import compression from "compression";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-
-<<<<<<< HEAD
-import * as parser from "../../dependencies/modelica-json/lib/parser";
 import { json } from "stream/consumers";
 
-import config from "./config";
-=======
 import fs from 'fs';
 import tmp from 'tmp';
 
-import * as parser from "./../dependencies/modelica-json/lib/parser";
+import config from "./config";
+import * as parser from "../../dependencies/modelica-json/lib/parser";
 
->>>>>>> 38613ec (Use tempfile with parser)
+
 
 const app = express();
 
@@ -32,14 +28,6 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-<<<<<<< HEAD
-app.post("/api/jsontomodelica", async (req, res) => {
-  // const jsonToConvert = req.body;
-  // // TODO: a better place to store converted modelica needs to be found.
-  // const modelica = parser.convertToModelica(jsonToConvert, "./modelica");
-
-  res.send("TODO: convert json to modelica");
-=======
 app.post('/api/jsontomodelica', async (req, res) => {
     const jsonToConvert = req.body;
     // 'tmp' module takes care of file cleanup
@@ -50,7 +38,6 @@ app.post('/api/jsontomodelica', async (req, res) => {
     const modelica = parser.convertToModelica(tmpFile.name, "./modelica");
 
     res.send(modelica);
->>>>>>> 38613ec (Use tempfile with parser)
 });
 
 app.post("/api/modelicatojson", async (req, res) => {
