5ee453a740418065bdeaff645543413ccc6cb229
Daren Keck <daren.keck@devetry.com>
2022-01-31 15:20:03 -0800

    When testing remove file paths

--- server/tests/integration/modelica-to-json.test.ts
+++ server/tests/integration/modelica-to-json.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 
 import { BUS_JSON, BUS_MODELICA } from "../static-data/modelica";
-import { stripSpacing } from "../utilities";
+import { stripFilePaths, stripSpacing } from "../utilities";
 
 const PORT = process.env.PORT ? process.env.port : 3000;
 const req = request(`http://localhost:${PORT}`);
@@ -15,7 +15,10 @@ describe("JSON to Modelica", () => {
       .expect(200)
       .then((res) => {
         expect(res).not.toBeNull();
-        expect(stripSpacing(res.text)).toBe(stripSpacing(JSON.stringify(BUS_JSON)));
+        const response = stripSpacing(stripFilePaths(JSON.parse(res.text)));
+        const expected = stripSpacing(stripFilePaths(BUS_JSON));
+
+        expect(response).toBe(expected);
       })
   });
 });
--- server/tests/utilities.ts
+++ server/tests/utilities.ts
@@ -8,3 +8,17 @@
 export const stripSpacing: (str: string) => string = (str) => {
     return str.replace(/[\s\r\n]+/g, '')
 };
+
+/**
+ * Translated files append an absolute path to where the file is stored on the server.
+ * Removes 'modelicaPath' and 'fullMoFilePath' as this path is randomized on the backend
+ * TODO: what are these file paths for? Can it be removed? Reporting absolute server
+ * paths back is probably not a good idea.
+ * @param json 
+ * @returns string 
+ */
+export const stripFilePaths: (o: any) => string = (o) => {
+    const {modelicaFile, fullMoFilePath, ...noPathsParsed} = o;
+
+    return JSON.stringify(noPathsParsed);
+}
