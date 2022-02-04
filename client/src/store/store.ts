34099cb04fd76539aed21876167b7d6334b1f927
Daren Keck <daren.keck@devetry.com>
2022-02-04 10:16:36 -0800

    Add interfaces, store accessor for mock data

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,6 +1,8 @@
 import create from "zustand";
 import { persist } from "zustand/middleware";
 
+import mockTemplates from "./system.json";
+
 export interface ProjectDetails {
   name: string;
   address: string;
@@ -11,9 +13,57 @@ export interface ProjectDetails {
   notes: string;
 }
 
+export interface SystemType {
+  id: number;
+  name: string;
+}
+
+export interface System {
+  id: number;
+  name: string;
+  systemType: number;
+  options?: number[];
+}
+
+export interface Option {
+  id: number;
+  type: 'dropdown' | 'string' | 'boolean' | 'number' | 'final';
+  name: string;
+  options?: Option[];
+}
+
+export interface SystemTemplates {
+  systemType: SystemType[];
+  system: System[];
+  options: Option[];
+}
+
+export interface Configuration {
+  id: number;
+  system: number; // ID of system
+  name: string;
+  configuration: any; // this is dynamically generated from modelica template selections
+}
+
+export interface MetaConfiguration {
+  tagPrefix: string;
+  tagStartIndex: number;
+  quantity: number;
+  configuration: number; // configuration ID
+}
+
+export interface UserProjects {
+  systems: System[];
+  configurations: Configuration[];
+  metaConfigurations: MetaConfiguration[];
+  schedules: any;
+}
+
 export interface State {
   projectDetails: Partial<ProjectDetails>;
   saveProjectDetails: (projectDetails: Partial<ProjectDetails>) => void;
+  templates?: SystemTemplates;
+  setTemplates: (templates: SystemTemplates) => void;
 }
 
 export const useStore = create<State>(
@@ -22,6 +72,8 @@ export const useStore = create<State>(
       projectDetails: {},
       saveProjectDetails: (projectDetails: Partial<ProjectDetails>) =>
         set(() => ({ projectDetails })),
+      setTemplates: (templates: Partial<SystemTemplates>) => 
+        set(() => {templates}),
     }),
     {
       name: "linkage-storage",
@@ -30,3 +82,6 @@ export const useStore = create<State>(
 );
 
 export const sanatizeStep = (step: number) => (step > 6 || step < 0 ? 0 : step);
+
+// intialize mock values
+useStore.setState({templates: mockTemplates as SystemTemplates});
