7901ae37b35168ae57dc586ca09ba56861892118
Daren Keck <daren.keck@devetry.com>
2022-02-04 16:06:40 -0800

    WIP: switch to inputs

--- client/src/components/steps/Systems.tsx
+++ client/src/components/steps/Systems.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import styled from "@emotion/styled";
 
 import { useStore, System, SystemType } from "../../store/store";
 
@@ -31,23 +32,50 @@ const ContentRight = () => {
 };
 
 const SystemGroupList = (typeList: SystemType[], systems: System[]) => {
-  const systemByTypeList: { [key: string]: string[] } = {};
+  // TODO: pass callback that matches the display text to the original
+  // system and that then calls the zustand set method`
   return typeList.map((type) =>
-    OptionList(
+    MultiSelect(
       type.name,
-      systems.filter((s) => s.systemType === type.id).map((s) => s.name),
+      systems.filter((s) => s.systemType === type.id).map((s) => {return {text: s.name, value: false};}),
+      (selection, value) => console.log(`${selection}: ${value}`)
     ),
   );
 };
 
-const OptionList = (title: string, options: string[]) => {
-  const optionList = options.map((option) => <li key="option">{option}</li>);
+const MultiSelect = (title: string, options: {text: string, value: boolean}[], handler: (s: string, value: boolean) => void) => {
+  // const optionList = options.map((option) => <li key="option">{option}</li>);
   return (
-    <Fragment>
-      <h3>{title}</h3>
-      <ul>{optionList}</ul>
+    <Fragment key={title}>
+      <div>
+        <h3>{title}</h3>
+        {options.map(option => OptionSelect(option.text, option.value, handler))}
+      </div>
     </Fragment>
   );
 };
+
+const OptionSelect = (text: string, value: boolean, handler: (text: string, value: boolean)=> void) => {
+  return (
+    <Fragment key={text}>
+      <div>
+        <OptionLabel htmlFor="selected">{text}</OptionLabel>
+        <Checkbox id="selected" name="selected" type="checkbox" checked={value} onChange={(e) => handler(text, e.target.checked)}></Checkbox>
+      </div>
+    </Fragment>
+  )
+};
+
+const OptionLabel = styled.label`
+  display: inline-block;
+  font-weight: bold;
+`;
+
+const Checkbox = styled.input`
+  display: inline-block;
+`
+const Container = styled.div`
+  width: 100%;
+`
 
 export default Systems;
