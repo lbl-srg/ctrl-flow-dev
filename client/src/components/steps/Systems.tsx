8269d6f6a5e268cae9e3745ac2961b9f138d63b6
Daren Keck <daren.keck@devetry.com>
2022-02-09 14:14:33 -0800

    WIP: continue work componetizing systems page

--- client/src/components/steps/Systems.tsx
+++ client/src/components/steps/Systems.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, ReactNode, FC } from "react";
 import styled from "@emotion/styled";
 
 import { useStore, System, SystemType } from "../../store/store";
@@ -18,86 +18,122 @@ const Systems = () => (
 
 const ContentLeft = () => {
   const { userSystems, systemTypes } = useStore((state) => 
-    ({userSystems: state.userProjects.systems, systemTypes: state.templates?.systemType}));
+    ({userSystems: state.userProjects.systems, systemTypes: state.systemTypes}));
 
   return (
-      <Fragment>
-        {systemTypes?.map(systemType =>
-          <UserSystemsNavGroup key={systemType.id}
-            systemType={systemType}
-            userSystems={userSystems?.filter(s => s.systemType === systemType.id)}
-          />
-        )}
-      </Fragment>
-
+    <SystemGroupList
+      systemTypes={systemTypes}
+      systems={userSystems}
+    />
   );
 };
 
-interface UserSystemsProps {
-  systemType: SystemType;
-  userSystems: System[] | undefined;
+interface SystemGroupListProps {
+  systemTypes: SystemType[];
+  systems: System[];
 }
-const UserSystemsNavGroup = ({systemType, userSystems}: UserSystemsProps) => {
+
+const SystemGroupList = ({systemTypes, systems}: SystemGroupListProps) => {
   return (
     <Fragment>
-      <a href={`#${systemType.name}`}><h3>{systemType.name}</h3></a>
-      { userSystems?.map(system => <UserSystem key={system.id} system={system} /> )}
+      {systemTypes?.map(systemType =>
+        <SystemGroup key={systemType.id}
+          systemType={systemType}
+          systems={systems?.filter(s => s.systemType === systemType.id)}
+        />
+      )}
     </Fragment>
   );
 }
 
-interface UserSystemProps {
+interface SystemGroupProps {
+  systemType: SystemType;
+  systems: System[];
+}
+
+const SystemGroup = ({systemType, systems}: SystemGroupProps) => {
+  return (
+    <Fragment>
+      <a href={`#${systemType.name}`}><h3>{systemType.name}</h3></a>
+      {systems.map(s => <UserSystem key={s.id} system={s} />)}
+    </Fragment>
+  );
+}
+
+interface SystemProp {
   system: System;
 }
 
-const UserSystem = ({system}: UserSystemProps) => {
-  return <a  key={system.id} href={`#${system.name}-${system.name}`}><li>{system.name}</li></a>;
+const UserSystem = ({system}: SystemProp) => {
+  return <a  key={system.id} href={`#${system.name}-${system.name}`}>
+          <li>{system.name}</li>
+        </a>;
 }
 
 const ContentRight = () => {
-  const templateSystems = useStore((state) => state.templates);
+  const { templateSystems, systemTypes } = useStore((state) => 
+  ({templateSystems: state.templates.systems, systemTypes: state.systemTypes}));
+
   return (
     <Fragment>
       <div>
         Select the systems types you will configure:
       </div>
-      {SystemGroupList(templateSystems.systemType, templateSystems.system)}
+      <TemplateGroupList 
+        systemTypes={systemTypes}
+        templateSystems={templateSystems}
+      />
     </Fragment>
   );
 };
 
-const SystemGroupList = (typeList: SystemType[], systems: System[]) => {
+interface TemplateGroupListProps {
+  systemTypes: SystemType[];
+  templateSystems: System[]
+}
+
+const TemplateGroupList = ({systemTypes, templateSystems}: TemplateGroupListProps) => {
   const userSystems = useStore(
     (state) => state.userProjects.systems,
   ) as System[];
   const addSystem = useStore((state) => state.addSystem);
   const removeSystem = useStore((state) => state.removeSystem);
 
-  return typeList.map((type) =>
-    MultiSelect(
-      type.name,
-      systems
-        .filter((s) => s.systemType === type.id)
-        .map((s) => {
-          const value =
-            userSystems?.findIndex((userS) => userS.id === s.id) >= 0;
-          return { text: s.name, value: value };
-        }),
-      (selection, value) => {
-        const system = systems.find((s) => s.name === selection);
-        if (system) {
-          value ? addSystem(system) : removeSystem(system);
-        }
-      },
-    ),
-  );
+  const handler = (selection: string, value: boolean)  => {
+    const system = userSystems.find((s) => s.name === selection);
+    if (system) {
+      value ? addSystem(system) : removeSystem(system);
+    } 
+  }
+
+  return <Fragment>
+    {
+      systemTypes.map((systemType) =>
+        <MultiSelect
+          key={systemType.id}
+          title={systemType.name}
+          options={
+            templateSystems.filter((s) => s.systemType === systemType.id)
+              .map((s) => {
+                const value =
+                  userSystems?.findIndex((userS) => userS.id === s.id) >= 0;
+                return { text: s.name, value: value };
+              })          
+          }
+          handler={handler}
+        />
+      )
+    }
+  </Fragment>
 };
 
-const MultiSelect = (
-  title: string,
-  options: { text: string; value: boolean }[],
-  handler: (s: string, value: boolean) => void,
-) => {
+interface MultiSelectProps {
+  title: string;
+  options: { text: string; value: boolean }[];
+  handler: (s: string, value: boolean) => void;
+}
+
+const MultiSelect = ({title, options, handler}: MultiSelectProps) => {
   const helpText =''; // TODO: extract from template
 
   return (
@@ -111,6 +147,14 @@ const MultiSelect = (
     </Fragment>
   );
 };
+
+interface OptionSelectProps {
+  text: string;
+  value: boolean;
+  helpText: string;
+  handler: (text: string, value: boolean) => void,
+  id?: string
+}
 
 const OptionSelect = (
   text: string,
