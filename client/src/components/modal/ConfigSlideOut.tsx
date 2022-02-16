f1c3f7cc37d0a3dacf928d5f503cf4a520114165
Daren Keck <daren.keck@devetry.com>
2022-02-16 13:50:50 -0800

    WIP: incorporate template options and user selections

--- client/src/components/modal/ConfigSlideOut.tsx
+++ client/src/components/modal/ConfigSlideOut.tsx
@@ -2,21 +2,41 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react/macro";
 import { Fragment, ReactNode, useState } from "react";
+import styled from "@emotion/styled";
+import { Field, Form, Formik } from "formik";
 
 import Button, { LinkButton } from "../Button";
 import { BaseModal, ModalOpenContext } from "./BaseModal";
 
-import { useStore, Configuration, System, State } from "../../store/store"
+import {
+  useStore,
+  Configuration,
+  System,
+  Option,
+  Selection,
+} from "../../store/store";
 
 interface SlideOutProps {
   template: System;
   config?: Configuration;
 }
 
-const SlideOut = ({template, config}: SlideOutProps) => {
+const SlideOut = ({ template, config }: SlideOutProps) => {
   const [isOpen, setOpen] = useState(false);
-  const { options } = useStore(state => ({ options: state.templates.options }));
-  const systemOptions = template.options ? template.options.map(optionId => options.find(o => o.id === optionId)) : [];
+  const { options } = useStore((state) => ({
+    options: state.templates.options,
+  }));
+  const systemOptions = template.options
+    ? (template.options.map((optionId) =>
+        options.find((o) => o.id === optionId),
+      ) as Option[])
+    : [];
+  const configSelections = config?.selections || [];
+  // pass in all selections with each option
+  // if the option has a selection, apply it
+  // check if option has options, if it does, render it
+  // if that section is another option, render that option, check for a selection, and apply it
+  // else if final, apply selection
 
   return (
     <ModalOpenContext.Provider value={isOpen}>
@@ -27,12 +47,61 @@ const SlideOut = ({template, config}: SlideOutProps) => {
           showCloseButton={false}
           css={slideOutCss}
         >
-          <h1>{template.name}</h1>
+          <h3>Configuration Name</h3>
+          <input></input>
+          {systemOptions.map((option) => (
+            <div key={option.id}>
+              <OptionDisplay
+                optionID={option.id}
+                options={options}
+                selections={configSelections}
+              />
+            </div>
+          ))}
         </BaseModal>
       </Fragment>
     </ModalOpenContext.Provider>
   );
 };
+
+interface OptionDisplayProps {
+  optionID: number;
+  options: Option[];
+  selections: Selection[];
+}
+
+const OptionDisplay = ({
+  optionID,
+  options,
+  selections,
+}: OptionDisplayProps) => {
+  const option = options.find((o) => o.id === optionID) as Option;
+  const selection = selections.find((s) => s.selection === optionID);
+  const optionList =
+    (option.options?.map((oID) =>
+      options.find((o) => o.id === oID),
+    ) as Option[]) || ([] as Option[]);
+
+  // TODO: will need to conditionally render the option based on 'type' (bool, value, dropdown)
+
+  return (
+    <Fragment>
+      <Label htmlFor={option.name}>{option.name}</Label>
+      <Field id={option.name} name={option.name}>
+        {optionList.map((o) => (
+          <option key={o.id} value={o.name}>
+            {o.name}
+          </option>
+        ))}
+      </Field>
+    </Fragment>
+  );
+};
+
+const Label = styled.label`
+  display: block;
+  font-weight: bold;
+`;
 
 const slideOutCss = css`
   right: 0; // force pane to the right hand side
