5d99afd8af91565c66a777dfd7f2e05748b44d6b
David Morrow <dperrymorrow@gmail.com>
2022-02-28 17:18:18 -0800

    rebuilt step nav

--- client/src/components/StepNavigation.tsx
+++ client/src/components/StepNavigation.tsx
@@ -1,192 +1,58 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx, css } from "@emotion/react/macro";
-import styled from "@emotion/styled/macro";
-import { Link, useMatch } from "react-router-dom";
-
-import { colors, dropShadow } from "../styleHelpers";
-import Button, { LinkButton } from "./Button";
-
-export const FOOTER_NAV_HEIGHT = "5rem";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/components/step-navigation.scss";
 
 const steps = [
-  "Landing",
-  "Details",
-  "Systems",
-  "Configs",
-  "Quantities",
-  "Schedules",
-  "Results",
+  { label: "Landing", path: "/" },
+  { label: "Details", path: "/details" },
+  { label: "Systems", path: "/systems" },
+  { label: "Configs", path: "/configs" },
+  { label: "Quantities", path: "/quantities" },
+  { label: "Schedules", path: "/schedules" },
+  { label: "Results", path: "/results" },
 ];
 
-const StepNavigation = () => {
-  return (
-    <NavContainer>
-      <NavItemContainer>
-        <BackButton />
-      </NavItemContainer>
-
-      <JumpNav />
-
-      <NavItemContainer
-        css={css`
-          text-align: right;
-        `}
-      >
-        <NextButton />
-      </NavItemContainer>
-    </NavContainer>
+function StepNavigation() {
+  const location = useLocation();
+  const currentIndex = steps.findIndex(
+    ({ path }) => location.pathname === path,
   );
-};
 
-const BackButton = () => {
-  const currentStep = useCurrentStep();
-
-  return (
-    <LinkButton variant="text" to={getStepPath(currentStep - 1)}>
-      ← Back {currentStep - 1 > 0 && `to ${steps[currentStep - 1]}`}
-    </LinkButton>
-  );
-};
-
-const NextButton = () => {
-  const currentStep = useCurrentStep();
-
-  if (currentStep + 1 > 6) {
-    return (
-      <Button
-        variant="filled"
-        onClick={() => alert("A download modal should pop up.")}
-      >
-        Download full project
-      </Button>
-    );
-  }
+  const previous = steps[currentIndex - 1];
+  const next = steps[currentIndex + 1];
 
   return (
-    <LinkButton to={getStepPath(currentStep + 1)} variant="filled">
-      Next Step: {steps[currentStep + 1]}
-    </LinkButton>
-  );
-};
+    <div className="step-navigation">
+      <div className="row">
+        <div className="col-xs-2">
+          <Link to={previous.path}>
+            <i className="icon-left-open" />
+            Back
+          </Link>
+        </div>
 
-const JumpNav = () => {
-  const currentStep = useCurrentStep();
-  const displaySteps = steps.slice(1);
+        <div className="col-xs-8 step-links">
+          {steps
+            .filter(({ path }) => path !== "/")
+            .map(({ label, path }) => {
+              return (
+                <Link key={label} to={path}>
+                  {label}
+                </Link>
+              );
+            })}
+        </div>
 
-  return (
-    <NavItemContainer
-      css={css`
-        min-width: ${displaySteps.length * 7}rem;
-      `}
-    >
-      <JumpNavContainer>
-        {displaySteps.map((stepName) => {
-          const stepIndex = steps.indexOf(stepName);
-          return (
-            <Link
-              css={css`
-                display: inline-block;
-                text-align: center;
-                width: 7rem;
-                height: 3.5rem;
-                flex-shrink: 0;
-                color: ${colors.black};
-                font-weight: bold;
-                text-decoration: none;
-              `}
-              key={stepName}
-              to={getStepPath(stepIndex)}
-            >
-              <Circle
-                css={
-                  stepIndex <= currentStep &&
-                  css`
-                    background: ${stepIndex == currentStep
-                      ? colors.black
-                      : colors.lightBlue};
-                  `
-                }
-              />
-              {stepName}
+        <div className="col-xs-2">
+          {next && (
+            <Link role="button" to={next.path}>
+              {next.label}
+              <i className="icon-right-open right" />
             </Link>
-          );
-        })}
-      </JumpNavContainer>
-      <CircleConnector />
-    </NavItemContainer>
+          )}
+        </div>
+      </div>
+    </div>
   );
-};
-
-/**
- * Accepts a step number and returns a correspoding URL
- * @param step the step number you are on. This will match an index in the `steps` constant
- * @returns the path that will take you to the correct step
- */
-const getStepPath = (step: number) => {
-  if (step > 0 && step <= 6) {
-    return `/${steps[step].toLowerCase()}`;
-  }
-
-  return "/";
-};
-
-/**
- * Custom hook that checks the current path and returns the corresponding step number
- * @returns the step number you are on. This will match an index in the `steps` constant
- */
-const useCurrentStep = () => {
-  for (const key in steps) {
-    if (useMatch(steps[key])) {
-      return parseInt(key);
-    }
-  }
-
-  // if no match is found, assume we're on the landing page
-  return 0;
-};
-
-const JumpNavContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const CircleConnector = styled.div`
-  margin: 0 4rem;
-  width: calc(100% - 8rem);
-  height: 4px;
-  background: ${colors.black};
-  position: relative;
-  top: -3rem;
-`;
-
-const Circle = styled.div`
-  margin: 0.1rem auto 0.5rem auto;
-  height: 0.8rem;
-  width: 0.8rem;
-  border-radius: 50%;
-  background: ${colors.white};
-  border: 2px solid ${colors.white};
-  outline: 2px solid ${colors.black};
-  position: relative;
-  z-index: 1;
-`;
-
-const NavContainer = styled.nav`
-  width: 100%;
-  height: ${FOOTER_NAV_HEIGHT};
-  position: relative;
-  z-index: 1;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  ${dropShadow}
-`;
-
-const NavItemContainer = styled.div`
-  max-width: 33%;
-  padding: 0 2rem;
-`;
+}
 
 export default StepNavigation;
