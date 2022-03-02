77359da0fa93ebe2a39f817a483da0b0267ec784
Daren Keck <daren.keck@devetry.com>
2022-03-02 10:45:09 -0800

    Add active project test, config add/remove test

--- client/src/store/store.test.ts
+++ client/src/store/store.test.ts
@@ -5,7 +5,7 @@ import {
     SystemTemplate
 } from "../store/store";
 
-test("Test denormalization", () => {
+test("Denormalization", () => {
     const [templateN, ...templatesN] = useStore.getState().templates;
     const template = useStore.getState().getTemplates().find(t => t.id === templateN.id) as SystemTemplate;
     const [_, options] = useStore.getState().getTemplateOptions(template);
@@ -13,8 +13,30 @@ test("Test denormalization", () => {
     expect(template).not.toBe(null || undefined);
     expect(templateN.id === template.id);
     const templateOptions = template.options as Option[];
-    console.log(templateOptions);
+
     templateOptions.map(o => {
         expect(o).toEqual(options.find(option => option.id === o.id));
     });
+});
+
+// TODO: expand this once actually switching between projects
+test("Active Project defaults to 1", () => { 
+    const project = useStore.getState().getActiveProject();
+
+    expect(project.id).toEqual(1);
+});
+
+test("Adding and Removing a Configuration", () => {
+    const [template, ...templates] = useStore.getState().getTemplates();
+    useStore.getState().addConfig(template, {name: "test"});
+
+    const [config, ...rest] = useStore.getState().getConfigs();
+    const [projectConfig, ...others] = useStore.getState().getActiveProject().configs;
+
+    expect(projectConfig).toEqual(config);
+
+    useStore.getState().removeConfig(projectConfig);
+
+    expect(useStore.getState().configurations.length).toBe(0);
+    expect(useStore.getState().getActiveProject().configs.length).toBe(0);
 });
