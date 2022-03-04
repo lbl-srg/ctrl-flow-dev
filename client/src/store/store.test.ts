f2a196bea2823847ff27cb8bef969e582c3f7a5e
Daren Keck <daren.keck@devetry.com>
2022-03-03 17:13:22 -0800

    Add addition store tests

--- client/src/store/store.test.ts
+++ client/src/store/store.test.ts
@@ -7,22 +7,6 @@ import {
 
 jest.mock('./mock-data');
 
-test("Denormalization", () => {
-    const [templateN, ...templatesN] = useStore.getState().templates;
-    const template = useStore.getState().getTemplates().find(t => t.id === templateN.id) as SystemTemplate;
-    const [_, options] = useStore.getState().getTemplateOptions(template);
-
-    expect(template).not.toBe(null || undefined);
-    expect(templateN.id === template.id);
-    const templateOptions = template.options as Option[];
-
-    templateOptions.map(o => {
-        expect(o).toEqual(options.find(option => option.id === o.id));
-    });
-});
-
-
-
 test("Adding and Removing a Configuration", () => {
     const testName = "test";
     const [template, ...templates] = useStore.getState().getTemplates();
@@ -61,21 +45,48 @@ test("Active Project defaults to 1", () => {
     expect(project.id).toEqual(1);
 });
 
-// NOTE: a reduced data set is being used to test option selection
-test("Test Setting Config Name", () => {
-    const testName = "ConfigTestName";
-    const options = useStore.getState().getOptions();
-    expect(options.length).toBe(7);
-    const [template1, templates] = useStore.getState().getTemplates();
+test("Template/Option Denormalization", () => {
+    const [templateN, ...templatesN] = useStore.getState().templates;
+    const template = useStore.getState().getTemplates().find(t => t.id === templateN.id) as SystemTemplate;
+    const [_, options] = useStore.getState().getTemplateOptions(template);
 
-    useStore.getState().addConfig(template1);
+    expect(template).not.toBe(null || undefined);
+    expect(templateN.id === template.id);
+    const templateOptions = template.options as Option[];
 
-    let[config, ...rest] = useStore.getState().getConfigs();
-    useStore.getState().updateConfig(config, testName, config.selections);
-    [config, ...rest] = useStore.getState().getConfigs();
-
-    expect(config.name).toEqual(testName);
+    templateOptions.map(o => {
+        expect(o).toEqual(options.find(option => option.id === o.id));
+    });
 });
+
+// NOTE: a reduced data set is being used to test option selection
+// test("Test Setting Config Name", () => {
+//     const testName = "ConfigTestName";
+//     const options = useStore.getState().getOptions();
+//     expect(options.length).toBe(7);
+//     const [template1, templates] = useStore.getState().getTemplates();
+
+//     useStore.getState().addConfig(template1);
+
+//     let[config, ...rest] = useStore.getState().getConfigs();
+//     useStore.getState().updateConfig(config, testName, config.selections);
+//     [config, ...rest] = useStore.getState().getConfigs();
+
+//     expect(config.name).toEqual(testName);
+// });
+
+/**
+ * Note: a smaller set of mocked data is being used for config testing
+ * 
+ * The tree of options available to select is:
+ * 
+ *               1
+ *             /    \
+ *            2       5
+ *           / \     / \
+ *          3   4   6   7
+ * 
+ */
 
 test("Test Updating Config Selections", () => {
     const testName = "ConfigTestName";
@@ -91,10 +102,10 @@ test("Test Updating Config Selections", () => {
         return previousValue;
     }, {})
 
-    // refer to mock data on tree of available options
+    // refer to mock data in __mocks__ folder for tree of available options
     const selections: Selection[] = [
-        {parent: optionMap[1], option: optionMap[2]},
-        {parent: optionMap[2], option: optionMap[3]}
+        {parent: optionMap[1], option: optionMap[2], value: undefined},
+        {parent: optionMap[2], option: optionMap[3], value: undefined}
     ]
 
     let[config, ...rest] = useStore.getState().getConfigs();
@@ -103,4 +114,45 @@ test("Test Updating Config Selections", () => {
 
     expect(config.name).toEqual(testName);
     expect(config.selections).toEqual(selections);
+
+});
+
+test("Test Config Selection Pruning", () => {
+    const testName = "ConfigTestName";
+    const options = useStore.getState().getOptions();
+    expect(options.length).toBe(7);
+    const [template1, templates] = useStore.getState().getTemplates();
+
+    useStore.getState().addConfig(template1);
+
+    // a map to help make option access a little easier:
+    const optionMap = options.reduce((previousValue: {[key: number]: Option}, currentValue: Option) => {
+        previousValue[currentValue.id] = currentValue;
+        return previousValue;
+    }, {})
+
+    let[config, ...rest] = useStore.getState().getConfigs();
+
+    // refer to mock data in __mocks__ folder for tree of available options
+    const selections: Selection[] = [
+        {parent: optionMap[1], option: optionMap[2], value: undefined},
+        {parent: optionMap[2], option: optionMap[3], value: undefined}
+    ]
+    useStore.getState().updateConfig(config, testName, selections);
+
+    // check that on selection of a different branch prunes out previous selctions
+    const newSelections: Selection[] = [
+        {parent: optionMap[1], option: optionMap[5], value: undefined}
+    ];
+    useStore.getState().updateConfig(config, testName, newSelections);
+    [config, ...rest] = useStore.getState().getConfigs();
+    expect(config.selections).toEqual(newSelections);
+
+    const addChildSelection: Selection[] = [
+        {parent: optionMap[5], option: optionMap[6], value: undefined}
+    ];
+    useStore.getState().updateConfig(config, testName, addChildSelection);
+    [config, ...rest] = useStore.getState().getConfigs();
+    // expect both selections (because both are on the same branch of options)
+    expect(config.selections).toEqual([...newSelections, ...addChildSelection]);
 });
