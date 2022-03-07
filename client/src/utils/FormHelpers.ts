2512268efd3f710db28b20c35c6547c743bea3ab
Daren Keck <daren.keck@devetry.com>
2022-03-07 10:22:28 -0800

    Format unformatted files

--- client/src/utils/FormHelpers.ts
+++ client/src/utils/FormHelpers.ts
@@ -1,39 +1,48 @@
 /**
  * Templates provide a tree of available options for a given system
- * 
+ *
  * This file has helper methods to help what options should be available and
  * submitted based on the user selction
  */
 
- import { getChangedValues } from "./utils";
+import { getChangedValues } from "./utils";
 
- import {
-    Configuration,
-    SystemTemplate,
-    Option,
-    Selection
+import {
+  Configuration,
+  SystemTemplate,
+  Option,
+  Selection,
 } from "../store/store";
 
-export type OptionRelation = {parent?: Option | undefined, children?: Option[] | undefined};
-export type ConfigFormValues = {[key: string]: number | string, configName: string} 
+export type OptionRelation = {
+  parent?: Option | undefined;
+  children?: Option[] | undefined;
+};
+export type ConfigFormValues = {
+  [key: string]: number | string;
+  configName: string;
+};
 
 /**
  * Traverses the tree of current selections for a given config and maps it
  * to a format convenient for formik
  */
-export const getInitialFormValues =
-    (template: SystemTemplate, config: Configuration, options: Option[]): {[key: number]: number | string} => {
-    const initValues: {[key: string]: number | string} = {};
-    const selections = config?.selections || [];
+export const getInitialFormValues = (
+  template: SystemTemplate,
+  config: Configuration,
+  options: Option[],
+): { [key: number]: number | string } => {
+  const initValues: { [key: string]: number | string } = {};
+  const selections = config?.selections || [];
 
-    selections.map(s => {
-      if (s.parent) {
-        initValues[s.parent.name] = s.option.id;
-      }
-    });
+  selections.map((s) => {
+    if (s.parent) {
+      initValues[s.parent.name] = s.option.id;
+    }
+  });
 
-    return initValues;
-}
+  return initValues;
+};
 
 /**
  * When given a set of new selections from formik, this helper method diffs the initial
@@ -42,20 +51,25 @@ export const getInitialFormValues =
  */
 export const getSelections = (
   values: ConfigFormValues,
-  initValues: {[key: string]: number},
-  options: Option[]): Selection[] => {
-  const { configName, ...confSelections } = values // extract out name
-  const newSelectionsFormValues = getChangedValues(confSelections, initValues) as ConfigFormValues;
+  initValues: { [key: string]: number },
+  options: Option[],
+): Selection[] => {
+  const { configName, ...confSelections } = values; // extract out name
+  const newSelectionsFormValues = getChangedValues(
+    confSelections,
+    initValues,
+  ) as ConfigFormValues;
 
   // build up new selections from changed selections
-  const newSelections: Selection[] = Object.entries(newSelectionsFormValues)
-    .map(([parentName, optionID]) => {
-      return {
-        parent: options.find(o => o.name === parentName),
-        option: options.find(o => o.id === optionID)
-      } as Selection
-    })
+  const newSelections: Selection[] = Object.entries(
+    newSelectionsFormValues,
+  ).map(([parentName, optionID]) => {
+    return {
+      parent: options.find((o) => o.name === parentName),
+      option: options.find((o) => o.id === optionID),
+    } as Selection;
+  });
 
   console.log(newSelectionsFormValues);
   return newSelections;
-}
+};
