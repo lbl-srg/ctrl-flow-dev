1fbf9b654ed746949821bc308874e4b715386d51
Daren Keck <daren.keck@devetry.com>
2022-03-07 14:56:17 -0800

    WIP: split out user store slice

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -10,17 +10,6 @@ import uiSlice, { uiSliceInterface } from "./slices/ui-slice";
 let _idIncrement = 0;
 const getID = () => (_idIncrement += 1);
 
-export interface ProjectDetails {
-  name: string;
-  address: string;
-  type: "multi-story office" | "warehouse" | "something else";
-  size: number;
-  units: "ip" | "something";
-  code: "ashrae 90.1 20201" | "a different one";
-  notes: string;
-  id: number;
-}
-
 export interface SystemType {
   id: number;
   name: string;
@@ -41,17 +30,6 @@ export interface Option extends Omit<OptionN, "options"> {
   options?: Option[];
 }
 
-export type SelectionN = {
-  parent: number;
-  option: number;
-  value?: number | boolean | string;
-};
-
-export interface Selection extends Omit<SelectionN, "parent" | "option"> {
-  parent: Option;
-  option: Option; // TODO: remove this 'option' key and add 'Option' as a possible type for value
-}
-
 export interface SystemTemplateN {
   id: number;
   systemType: number;
@@ -63,58 +41,6 @@ export interface SystemTemplate
   extends Omit<SystemTemplateN, "systemType" | "options"> {
   systemType: SystemType;
   options?: Option[];
-}
-
-export interface ConfigurationN {
-  id: number;
-  template: number; // ID of SystemTemplate
-  name: string | undefined;
-  selections: SelectionN[];
-}
-
-export interface Configuration
-  extends Omit<ConfigurationN, "template" | "selections"> {
-  template: SystemTemplate;
-  selections: Selection[];
-}
-
-export interface AppliedConfigurationN {
-  id: number;
-  tag: string;
-  config: number;
-  data: any[]; // TODO: how we get the types for the rest of the table data is still being defined
-}
-
-export interface AppliedConfiguration
-  extends Omit<AppliedConfigurationN, "config"> {
-  config: Configuration;
-}
-
-export interface MetaConfigurationN {
-  id: number;
-  tagPrefix: string;
-  tagStartIndex: number;
-  quantity: number;
-  config: number; // configuration ID
-}
-
-export interface MetaConfiguration extends Omit<MetaConfigurationN, "config"> {
-  config: Configuration;
-}
-
-export interface UserProjectN {
-  configs: number[];
-  metaConfigs: number[];
-  appliedConfigs: number[];
-  projectDetails: Partial<ProjectDetails>;
-  id: number;
-}
-
-export interface UserProject
-  extends Omit<UserProjectN, "configs" | "metaConfigs" | "appliedConfigs"> {
-  configs: Configuration[];
-  metaConfigs: MetaConfiguration[];
-  appliedConfigs: AppliedConfiguration[];
 }
 
 type GetAction<T> = (get: GetState<State>) => T;
