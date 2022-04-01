b97f9f2afc79bcfb58abc5a8be0eba233f940b26
Daren Keck <daren.keck@devetry.com>
2022-04-01 10:54:09 -0700

    Update default modelica dependencies path

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,5 +3,6 @@ import "dotenv/config";
 export default {
   NODE_ENV: process.env.NODE_ENV || "production",
   PORT: process.env.PORT || 3000,
-  MODELICA_DEPENDENCIES: process.env.MODELICA_DEPENDENCIES || "/dependencies",
+  MODELICA_DEPENDENCIES:
+    process.env.MODELICA_DEPENDENCIES || "../../dependencies",
 };
