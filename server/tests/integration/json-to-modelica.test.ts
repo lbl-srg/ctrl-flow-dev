4c52867137b8962515d0890289a328b3f5efc26b
Daren Keck <daren.keck@devetry.com>
2022-06-15 10:32:04 -0700

    Skip API integration tests

--- server/tests/integration/json-to-modelica.test.ts
+++ server/tests/integration/json-to-modelica.test.ts
@@ -6,16 +6,16 @@ import { stripSpacing } from "../utilities";
 const PORT = process.env.PORT ? process.env.port : 3000;
 const req = request(`http://localhost:${PORT}`);
 
-describe("JSON to Modelica", () => {
+describe.skip("JSON to Modelica", () => {
   it("Converts JSON to Modelica", () => {
     req
-      .post('/api/jsontomodelica')
+      .post("/api/jsontomodelica")
       .send(BUS_JSON)
-      .set('Accept', 'application/json')
+      .set("Accept", "application/json")
       .expect(200)
       .then((res) => {
         expect(res).not.toBeNull();
         expect(stripSpacing(res.text)).toBe(stripSpacing(BUS_MODELICA));
-      })
+      });
   });
 });
