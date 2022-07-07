095c2cc761d5b407e5d44e8e553a6a7dc9178229
Yves Gurcan <gurcan.yves+github@gmail.com>
2022-07-07 15:07:58 -0700

    Generate docx from tex file

--- server/src/sequence/index.ts
+++ server/src/sequence/index.ts
@@ -1,10 +1,8 @@
-import fs from "fs/promises";
 import path from "path";
 import _ from "underscore";
-import marked from "marked";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import HTMLtoDOCX from "html-to-docx";
+import pandoc from "node-pandoc-promise";
 
 // TODO: Replace this mock type definition with the actual parameters necessary to generate the document
 export type TemplateOptions = {
@@ -12,45 +10,35 @@ export type TemplateOptions = {
   dual_inlet_airflow_sensors: string;
 };
 
-const MARKDOWN_TEMPLATE_FILE_PATH = `${path.resolve(__dirname)}/template.md`;
+const SEQUENCE_DOCUMENTATION_TEMPLATE_PATH = `${path.resolve(
+  __dirname,
+)}/template.tex`;
+const SEQUENCE_DOCUMENTATION_TEMPLATE_FORMAT = "latex";
+const SEQUENCE_DOCUMENTATION_OUTPUT_PARTIAL_PATH = `${path.resolve(
+  __dirname,
+)}/SequenceDocumentation-`;
+const SEQUENCE_DOCUMENTATION_OUTPUT_EXTENSION = ".docx";
 
-// TODO: Check that the copy below fits the client's needs
-const HTML_HEADER = `<p>&copy; ASHRAE (www.ashrae.org). For personal use only. Additional reproduction, distribution, or transmission in either print or digital form is not permitted without ASHRAE's prior written permission.</p>`;
-
-// TODO: Check that the copy below fits the client's needs
-const HTML_FOOTER = `<p>ASHRAE Guideline 36-2018</p>`;
-
-const DOCUMENT_OPTIONS = {};
-
-async function getTemplate() {
-  try {
-    return fs.readFile(MARKDOWN_TEMPLATE_FILE_PATH, {
-      encoding: "utf8",
-    });
-  } catch (err) {
-    console.log(err);
-    return;
-  }
+async function generateDocument() {
+  const timeMarker = new Date().toISOString();
+  const destinationFile = `${SEQUENCE_DOCUMENTATION_OUTPUT_PARTIAL_PATH}${timeMarker}${SEQUENCE_DOCUMENTATION_OUTPUT_EXTENSION}`;
+  const pandocArguments =
+    `-f ${SEQUENCE_DOCUMENTATION_TEMPLATE_FORMAT} -o ${destinationFile}`.split(
+      " ",
+    );
+  console.log(
+    "Running Pandoc with the following arguments:",
+    SEQUENCE_DOCUMENTATION_TEMPLATE_PATH,
+    pandocArguments.join(" "),
+  );
+  await pandoc(SEQUENCE_DOCUMENTATION_TEMPLATE_PATH, pandocArguments);
 }
 
-// TODO: As we figure out parameters to create the document, add business logic here
-function configureTemplateOptions(parameters: TemplateOptions) {
-  return parameters;
-}
+export async function writeSequenceDocumentation(parameters: TemplateOptions) {
+  const document = await generateDocument();
+  return document;
 
-async function fillInTemplate(
-  template: string,
-  templateOptions: TemplateOptions,
-) {
-  return _.template(template)(templateOptions);
-}
-
-async function generateDocument(filledInTemplate: string) {
-  const htmlBody = marked(filledInTemplate);
-  return HTMLtoDOCX(htmlBody, HTML_HEADER, DOCUMENT_OPTIONS, HTML_FOOTER);
-}
-
-export default async function (parameters: TemplateOptions) {
+  /*
   const template = await getTemplate();
   if (!template) {
     // TODO: Return an error so that the frontend can communicate to the user that something went wrong
@@ -60,4 +48,5 @@ export default async function (parameters: TemplateOptions) {
   const templateOptions = configureTemplateOptions(parameters);
   const filledInTemplate = await fillInTemplate(template, templateOptions);
   return generateDocument(filledInTemplate);
+  */
 }
