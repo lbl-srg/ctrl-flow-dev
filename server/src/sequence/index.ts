34e4099a179fd4555cdb1618cfd060541c01a072
Yves Gurcan <gurcan.yves+github@gmail.com>
2022-07-12 16:11:18 -0700

    Implement conversion from LaTex to ODT to DOCX.

--- server/src/sequence/index.ts
+++ server/src/sequence/index.ts
@@ -1,52 +1,116 @@
+import fs from "fs/promises";
 import path from "path";
+import util from "util";
+import process from "process";
+import { exec } from "child_process";
 import _ from "underscore";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import pandoc from "node-pandoc-promise";
 
-// TODO: Replace this mock type definition with the actual parameters necessary to generate the document
-export type TemplateOptions = {
-  optional: boolean;
-  dual_inlet_airflow_sensors: string;
-};
+// Enables the use of async/await keywords when executing external processes
+const execPromise = util.promisify(exec);
 
-const SEQUENCE_DOCUMENTATION_TEMPLATE_PATH = `${path.resolve(
-  __dirname,
-)}/template.tex`;
-const SEQUENCE_DOCUMENTATION_TEMPLATE_FORMAT = "latex";
-const SEQUENCE_DOCUMENTATION_OUTPUT_PARTIAL_PATH = `${path.resolve(
-  __dirname,
-)}/SequenceDocumentation-`;
-const SEQUENCE_DOCUMENTATION_OUTPUT_EXTENSION = ".docx";
-
-async function generateDocument() {
-  const timeMarker = new Date().toISOString();
-  const destinationFile = `${SEQUENCE_DOCUMENTATION_OUTPUT_PARTIAL_PATH}${timeMarker}${SEQUENCE_DOCUMENTATION_OUTPUT_EXTENSION}`;
-  const pandocArguments =
-    `-f ${SEQUENCE_DOCUMENTATION_TEMPLATE_FORMAT} -o ${destinationFile}`.split(
-      " ",
-    );
-  console.log(
-    "Running Pandoc with the following arguments:",
-    SEQUENCE_DOCUMENTATION_TEMPLATE_PATH,
-    pandocArguments.join(" "),
-  );
-  await pandoc(SEQUENCE_DOCUMENTATION_TEMPLATE_PATH, pandocArguments);
+export enum EnergyCode {
+  Ashrae = "ashrae",
+  California = "california",
 }
 
-export async function writeSequenceDocumentation(parameters: TemplateOptions) {
-  const document = await generateDocument();
-  return document;
+export type ControlSequenceInput = {
+  energyCode: EnergyCode;
+  choices: {
+    BuildingsTemplatesAirHandlersFansInterfacesPartialAirHandlertypFanRet: string;
+  };
+};
 
-  /*
-  const template = await getTemplate();
-  if (!template) {
-    // TODO: Return an error so that the frontend can communicate to the user that something went wrong
-    return;
-  }
+async function writeLatexFile(
+  controlSequenceInput: ControlSequenceInput,
+  latexFilePath: string,
+) {
+  const { energyCode, choices } = controlSequenceInput;
+  const {
+    BuildingsTemplatesAirHandlersFansInterfacesPartialAirHandlertypFanRet,
+  } = choices;
 
-  const templateOptions = configureTemplateOptions(parameters);
-  const filledInTemplate = await fillInTemplate(template, templateOptions);
-  return generateDocument(filledInTemplate);
-  */
+  const latexFileContent = String.raw`
+    % Below are commands that define the input for the Control Sequence Document.
+    % These commands are named after the modelica path of the option they represent without dot characters (e.g., \BuildingsTemplatesAirHandlersFansInterfacesPartialAirHandlertypFanRet).
+    % These commands return the modelica path of the selected value with dot characters (e.g., Buildings.Templates.Components.Types.Fan.SingleConstant).
+    % These commands are used to decide which parts of the Control Sequence Document are displayed.
+    
+    % Type of return fan
+    
+    ${
+      BuildingsTemplatesAirHandlersFansInterfacesPartialAirHandlertypFanRet &&
+      String.raw`\newcommand\BuildingsTemplatesAirHandlersFansInterfacesPartialAirHandlertypFanRet{${BuildingsTemplatesAirHandlersFansInterfacesPartialAirHandlertypFanRet}}`
+    }
+    
+    % Inject LaTeX template for the Control Sequence Document.
+    
+    \input{${path.resolve(__dirname)}/template-${energyCode}.tex}
+  `;
+
+  return fs.writeFile(latexFilePath, latexFileContent);
+}
+
+// Pandoc does not support all LaTeX functionalities that other compilers support
+// (e.g., https://github.com/jgm/pandoc/issues/8029, https://github.com/jgm/pandoc/issues/7757, https://github.com/jgm/pandoc/issues/8027).
+// The result of converting LaTeX files to Microsoft Word Documents with Pandoc are pretty unpredictable as to what will work as expected or not.
+// So we use make4ht as an intermediary to create an Open Office Document that we then convert into a Microsoft Word Document with Pandoc.
+async function convertToODT(
+  latexFilePath: string,
+  odtFilePath: string,
+  odtRootFilePath: string,
+  tempOdtRootFilePath: string,
+) {
+  const conversionResult = await execPromise(`make4ht -f odt ${latexFilePath}`);
+  // Moves the .odt file to the output folder.
+  await fs.rename(odtRootFilePath, odtFilePath);
+  // Removes temporary file at the root of the server folder.
+  await execPromise(`rm ${tempOdtRootFilePath}.*`);
+  return conversionResult;
+}
+
+async function convertToDOCX(odtFilePath: string, docxFilePath: string) {
+  const pandocArguments = `-f odt -o ${docxFilePath}`.split(" ");
+
+  console.log(
+    "Running Pandoc with the following arguments:",
+    odtFilePath,
+    pandocArguments.join(" "),
+  );
+
+  await pandoc(odtFilePath, pandocArguments);
+}
+
+async function getConvertedDocument(convertedDocumentPath: string) {
+  const file = await fs.readFile(convertedDocumentPath);
+  return file;
+}
+
+export async function writeControlSequenceDocument(
+  controlSequenceInput: ControlSequenceInput,
+) {
+  const timeMarker = new Date().toISOString();
+  const fileName = `sequence-${timeMarker}`;
+
+  const rootPath = process.cwd();
+  const sequencePath = path.resolve(__dirname);
+  const outputPath = `${sequencePath}/output-documents`;
+
+  const latexFilePath = `${outputPath}/${fileName}.tex`;
+  const odtRootFilePath = `${rootPath}/${fileName}.odt`;
+  const odtFilePath = `${outputPath}/${fileName}.odt`;
+  const tempOdtRootFilePath = `${rootPath}/${fileName}`;
+  const docxFilePath = `${outputPath}/${fileName}.docx`;
+
+  await writeLatexFile(controlSequenceInput, latexFilePath);
+  await convertToODT(
+    latexFilePath,
+    odtFilePath,
+    odtRootFilePath,
+    tempOdtRootFilePath,
+  );
+  await convertToDOCX(odtFilePath, docxFilePath);
+  return getConvertedDocument(docxFilePath);
 }
