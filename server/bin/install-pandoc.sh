f6424c0562fd02e048ecebfc751f299953a004b0
Yves Gurcan <gurcan.yves+github@gmail.com>
2022-07-20 19:33:38 -0700

    Sudo twice

--- server/bin/install-pandoc.sh
+++ server/bin/install-pandoc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
 
-sudo apt-get --assume-yes update && apt-get --assume-yes install wget pandoc=2.9.2.1-3ubuntu2 texlive=2021.20220204-1 texlive-extra-utils=2021.20220204-1
+sudo apt-get --assume-yes update && sudo apt-get --assume-yes install wget pandoc=2.9.2.1-3ubuntu2 texlive=2021.20220204-1 texlive-extra-utils=2021.20220204-1
