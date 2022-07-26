c5f638fbf8ed1a7b82cb9e335641e6c621b6acf0
Yves Gurcan <gurcan.yves+github@gmail.com>
2022-07-26 13:57:11 -0700

    Update unit tests

--- server/tests/unit/sequence/index.test.ts
+++ server/tests/unit/sequence/index.test.ts
@@ -23,30 +23,14 @@ describe("Control Sequence Document", () => {
     await writeLatexFile(input, latexFilePath);
   });
 
-  it("convertToODT executes without error", async () => {
-    const rootPath = process.cwd();
-    const sequencePath = `${process.cwd()}/src/sequence`;
-    const outputPath = `${sequencePath}/output-documents`;
-    const latexFilePath = `${outputPath}/unit-test.tex`;
-    const odtFilePath = `${outputPath}/unit-test.odt`;
-    const odtRootFilePath = `${rootPath}/unit-test.odt`;
-    const tempOdtRootFilePath = `${rootPath}/unit-test`;
-    await convertToODT(
-      latexFilePath,
-      odtFilePath,
-      odtRootFilePath,
-      tempOdtRootFilePath,
-    );
-  });
-
   it(
     "convertToDOCX executes without error",
     async () => {
       const sequencePath = `${process.cwd()}/src/sequence`;
       const outputPath = `${sequencePath}/output-documents`;
-      const odtFilePath = `${outputPath}/unit-test.odt`;
+      const latexFilePath = `${outputPath}/unit-test.tex`;
       const docxFilePath = `${outputPath}/unit-test.docx`;
-      await convertToDOCX(odtFilePath, docxFilePath);
+      await convertToDOCX(latexFilePath, docxFilePath);
     },
     TIMEOUT_IN_MILLISECONDS,
   );
