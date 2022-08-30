fa0f1325347d23d271e28f80452d790b84c68c0d
Daren Keck <daren.keck@deptagency.com>
2022-08-30 10:38:54 -0700

    Start to integrate mods with tree visualizer

--- server/scripts/visualize-option-tree.js
+++ server/scripts/visualize-option-tree.js
@@ -13,23 +13,29 @@ const optionMap = {};
 options.map((o) => (optionMap[o.modelicaPath] = o));
 
 const isPrimitive = (o) => {
-  const type = o.type;
+  const type = o?.type;
   return (
-    type && (type.startsWith("Medium") || ["Boolean", "String"].includes(type))
+    type &&
+    (type.startsWith("Medium") || ["String", "Integer", "Real"].includes(type))
   );
 };
 
-const writeNode = (nodePath, depth = 0) => {
+const writeNode = (nodePath, mods = {}, depth = 0) => {
   const node = optionMap[nodePath];
-  let printStr = node?.visible ? `******* - ${node?.name}` : `${node?.name}`;
-  // if (node?.visible && !isPrimitive(node)) {
-  console.log(DebugTree.depth(depth), printStr);
-  // }
+  const type = node?.type;
+  const mod = type ? mods[type] : null;
+  const visible =
+    mod?.final !== undefined ? node?.visible && mod.final : node?.visible;
+  // check the mod back for final
+  let printStr = visible ? `** - ${node?.name}` : `${node?.name}`;
+  if (!isPrimitive(node)) {
+    console.log(DebugTree.depth(depth), printStr);
+  }
 
   node?.options?.map((oPath) => {
     const o = optionMap[oPath];
-    const newDepth = o?.visible ? depth + 1 : depth;
-    writeNode(oPath, depth + 1);
+    const newMods = o?.modifiers ? { ...o.modifiers, ...mods } : mods;
+    writeNode(oPath, newMods, depth + 1);
   });
 };
 
