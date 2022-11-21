cf01e5e6a9de7cf4156b14c942170fa71de1f7b3
Daren Keck <daren.keck@deptagency.com>
2022-11-21 12:05:16 -0800

    Leave stub method in sequence doc unit tests

--- server/tests/unit/sequence/index.test.ts
+++ server/tests/unit/sequence/index.test.ts
@@ -8,28 +8,16 @@ import {
 const TIMEOUT_IN_MILLISECONDS = 30000;
 
 describe("Control Sequence Document", () => {
-  // it("writeLatexFile executes without error", async () => {
-  //   const input: ControlSequenceInput = {
-  //     energyCode: EnergyCode.Ashrae,
-  //     choices: {
-  //       BuildingsTemplatesAirHandlersFansInterfacesPartialAirHandlertypFanRet:
-  //         "Buildings.Templates.Components.Types.Fan.SingleConstant",
-  //     },
-  //   };
-  //   const sequencePath = `${process.cwd()}/src/sequence`;
-  //   const outputPath = `${sequencePath}/output-documents`;
-  //   const latexFilePath = `${outputPath}/unit-test.tex`;
-  //   await writeLatexFile(input, latexFilePath);
-  // });
-  // it(
-  //   "convertToDOCX executes without error",
-  //   async () => {
-  //     const sequencePath = `${process.cwd()}/src/sequence`;
-  //     const outputPath = `${sequencePath}/output-documents`;
-  //     const latexFilePath = `${outputPath}/unit-test.tex`;
-  //     const docxFilePath = `${outputPath}/unit-test.docx`;
-  //     await convertToDOCX(latexFilePath, docxFilePath);
-  //   },
-  //   TIMEOUT_IN_MILLISECONDS,
-  // );
+  it(
+    "convertToDOCX executes without error",
+    async () => {
+      // const sequencePath = `${process.cwd()}/src/sequence`;
+      // const outputPath = `${sequencePath}/output-documents`;
+      // const latexFilePath = `${outputPath}/unit-test.tex`;
+      // const docxFilePath = `${outputPath}/unit-test.docx`;
+      // await convertToDOCX(latexFilePath, docxFilePath);
+      return;
+    },
+    TIMEOUT_IN_MILLISECONDS,
+  );
 });
