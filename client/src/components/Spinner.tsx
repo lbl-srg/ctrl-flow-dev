b1502bd06bac4ce3d85b778afb273e3743ec7cfb
Amit Kapoor <amit@devetry.com>
2022-12-22 11:41:17 -0700

    fix tabbing

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -1,22 +1,21 @@
 import "../styles/components/spinner.scss";
 
 export interface SpinnerProps {
-	loading: boolean;
-	text: string;
+  loading: boolean;
+  text: string;
 }
 
 function Spinner({ loading, text }: SpinnerProps) {
-	console.log('spinner loading: ', loading);
-	if (!loading) return null;
+  if (!loading) return null;
 
-	return (
-		<div className="loader-container">
-			<div className="loader"></div>
-			<div className="loader-text">
-				{text}
-			</div>
-		</div>
-	);
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+      <div className="loader-text">
+        {text}
+      </div>
+    </div>
+  );
 }
 
 export default Spinner;
