fb5058f2be803da9843bf40385b2676f676d4358
Daren Keck <daren.keck@deptagency.com>
2022-12-27 16:44:23 -0800

    Typing and white space adjustments

--- client/src/components/steps/Configs/Config.tsx
+++ client/src/components/steps/Configs/Config.tsx
@@ -9,7 +9,7 @@ import { useStores } from "../../../data";
 import { OptionInterface, TemplateInterface } from "../../../data/template";
 import { Modifiers, ConfigValues } from "../../../utils/modifier-helpers";
 import { ConfigInterface } from "../../../data/config";
-import Spinner from '../../Spinner';
+import Spinner from "../../Spinner";
 
 export interface ConfigProps {
   configId: string | undefined;
@@ -17,64 +17,62 @@ export interface ConfigProps {
   projectEvaluatedValues: ConfigValues;
 }
 
-const Config = observer(({ configId, projectSelections, projectEvaluatedValues }: ConfigProps) => {
-  const { configStore, templateStore, uiStore } = useStores();
-  const config = configStore.getById(configId) as ConfigInterface;
-  const template = templateStore.getTemplateByPath(
-    config.templatePath,
-  ) as TemplateInterface;
-  const templateOptions: OptionInterface[] =
-    templateStore.getOptionsForTemplate(template?.modelicaPath);
-  const templateModifiers: Modifiers = templateStore.getModifiersForTemplate(
-    template?.modelicaPath,
-  );
-  const selections: ConfigValues = configStore.getConfigSelections(configId);
-  const allOptions: { [key: string]: OptionInterface } =
-    templateStore.getAllOptions();
+const Config = observer(
+  ({ configId, projectSelections, projectEvaluatedValues }: ConfigProps) => {
+    const { configStore, templateStore, uiStore } = useStores();
+    const config = configStore.getById(configId) as ConfigInterface;
+    const template = templateStore.getTemplateByPath(
+      config.templatePath,
+    ) as TemplateInterface;
+    const templateOptions: OptionInterface[] =
+      templateStore.getOptionsForTemplate(template?.modelicaPath);
+    const templateModifiers: Modifiers = templateStore.getModifiersForTemplate(
+      template?.modelicaPath,
+    );
+    const selections: ConfigValues = configStore.getConfigSelections(configId);
+    const allOptions: { [key: string]: OptionInterface } =
+      templateStore.getAllOptions();
 
-  const [openedModal, setOpenedModal] = useState<boolean>(false);
-  const [isLoading, setLoading] = useState<boolean>(false);
+    const [openedModal, setOpenedModal] = useState<boolean>(false);
+    const [isLoading, setLoading] = useState<boolean>(false);
 
-  function removeConfiguration(event: MouseEvent) {
-    event.preventDefault();
-    configStore.remove(configId);
-  }
-
-  const updateName = useDebouncedCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      configStore.update(config.id, { name: event.target.value });
-    },
-    400,
-  );
-
-  function openModal() {
-    setLoading(true);
-  }
-
-  useEffect(() => {
-    if (isLoading && !openedModal) {
-      setOpenedModal(true);
-      uiStore.setOpenSystemPath(config.systemPath);
+    function removeConfiguration(event: MouseEvent) {
+      event.preventDefault();
+      configStore.remove(configId);
     }
-  }, [isLoading]);
 
-  return (
-    <Fragment>
-      <Spinner
-        loading={isLoading}
-        text="Please wait..."
-      />
-      <div className="config" id={`config-${configId}`} data-spy="config">
-        <div className="input-container">
-          <input
-            type="text"
-            onChange={updateName}
-            placeholder="Enter Configuration Name"
-            defaultValue={config.name}
-            disabled={config.isLocked}
-          />
-          <div className="config-actions">
-            {/*<i
+    const updateName = useDebouncedCallback(
+      (event: ChangeEvent<HTMLInputElement>) => {
+        configStore.update(config.id, { name: event.target.value });
+      },
+      400,
+    );
+
+    function openModal() {
+      setLoading(true);
+    }
+
+    useEffect(() => {
+      if (isLoading && !openedModal) {
+        setOpenedModal(true);
+        uiStore.setOpenSystemPath(config.systemPath);
+      }
+    }, [isLoading]);
+
+    return (
+      <Fragment>
+        <Spinner loading={isLoading} text="Please wait..." />
+        <div className="config" id={`config-${configId}`} data-spy="config">
+          <div className="input-container">
+            <input
+              type="text"
+              onChange={updateName}
+              placeholder="Enter Configuration Name"
+              defaultValue={config.name}
+              disabled={config.isLocked}
+            />
+            <div className="config-actions">
+              {/*<i
               onClick={() => configStore.toggleConfigLock(configId)}
               className={
                 config.isLocked
@@ -82,33 +80,34 @@ const Config = observer(({ configId, projectSelections, projectEvaluatedValues }
                   : "lock-toggle icon-lock-open"
               }
             />*/}
-            <SlideOutOpenButton
-              disabled={config.isLocked}
-              onClick={openModal}
-            />
+              <SlideOutOpenButton
+                disabled={config.isLocked}
+                onClick={openModal}
+              />
+            </div>
           </div>
+          <a href="#" className="remove" onClick={removeConfiguration}>
+            <i className="icon-close" />
+          </a>
         </div>
-        <a href="#" className="remove" onClick={removeConfiguration}>
-          <i className="icon-close" />
-        </a>
-      </div>
-      {openedModal && (
-        <SlideOut
-          config={config}
-          projectSelections={projectSelections}
-          projectEvaluatedValues={projectEvaluatedValues}
-          template={template}
-          templateOptions={templateOptions} 
-          templateModifiers={templateModifiers}
-          selections={selections}
-          allOptions={allOptions}
-          // startLoading={() => setLoading(true)}
-          stopLoading={() => setLoading(false)}
-          close={() => setOpenedModal(false)}
-        />
-      )}
-    </Fragment>
-  );
-});
+        {openedModal && (
+          <SlideOut
+            config={config}
+            projectSelections={projectSelections}
+            projectEvaluatedValues={projectEvaluatedValues}
+            template={template}
+            templateOptions={templateOptions}
+            templateModifiers={templateModifiers}
+            selections={selections}
+            allOptions={allOptions}
+            // startLoading={() => setLoading(true)}
+            stopLoading={() => setLoading(false)}
+            close={() => setOpenedModal(false)}
+          />
+        )}
+      </Fragment>
+    );
+  },
+);
 
 export default Config;
