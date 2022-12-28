9f6a41ebc099e008f6848ef3e3a13a452bbfce80
Daren Keck <daren.keck@deptagency.com>
2022-12-28 14:40:30 -0800

    Remove debug method, add documentation

--- client/src/utils/modifier-helpers.ts
+++ client/src/utils/modifier-helpers.ts
@@ -300,6 +300,33 @@ export function applyVisibilityModifiers(
   return !!(visible && enable);
 }
 
+/**
+ * Replaceable components can be swapped by either:
+ * - having a selection made by the user
+ * - a redeclared modifier
+ *
+ * Modifiers are collected by traversing the tree of related options, grabbing
+ * each visited options modifiers and putting it in the global modifiers object
+ *
+ * Selections or redeclare modifiers decide which branch to take for the specified
+ * node, overwriting any previous modifier values that might have been present
+ *
+ * e.g.
+ *
+ *             A         // 'A' is a replaceable component
+ *            /  \
+ *           B    C __   // 'B' and 'C' are choices
+ *          / \    \   \
+ *         D=1 E=2  D=3  E=4
+ *
+ * The default value (what is present in `option.value`) might point to the choice B, which means all of
+ * 'B's downstream values for D and E are included ('1' and '2')
+ *
+ * If 'C' is selected, all of its downstream values need to be in the modifier object,
+ * meaing D has a value of '3' and E '4'
+ *
+ * Also: a redeclare modifier (not included as a user selection) has the same behavior
+ */
 export function getUpdatedModifiers(
   values: ConfigValues,
   modifiers: Modifiers,
@@ -313,6 +340,7 @@ export function getUpdatedModifiers(
       const [modelicaPath, instancePath] = key.split("-");
       const option = allOptions[modelicaPath] as OptionInterface;
 
+      // Merge and overwrite based on the selected/redeclared option
       updatedModifiers = {
         ...updatedModifiers,
         ...buildModifiers(
@@ -328,80 +356,3 @@ export function getUpdatedModifiers(
 
   return updatedModifiers;
 }
-
-export const deepDiffMapper = (function () {
-  return {
-    VALUE_CREATED: "created",
-    VALUE_UPDATED: "updated",
-    VALUE_DELETED: "deleted",
-    VALUE_UNCHANGED: "unchanged",
-    map: function (obj1: any, obj2: any) {
-      if (this.isFunction(obj1) || this.isFunction(obj2)) {
-        throw "Invalid argument. Function given, object expected.";
-      }
-      if (this.isValue(obj1) || this.isValue(obj2)) {
-        return {
-          type: this.compareValues(obj1, obj2),
-          data: obj1 === undefined ? obj2 : obj1,
-        };
-      }
-
-      const diff: any = {};
-      for (const key in obj1) {
-        if (this.isFunction(obj1[key])) {
-          continue;
-        }
-
-        let value2 = undefined;
-        if (obj2[key] !== undefined) {
-          value2 = obj2[key];
-        }
-
-        diff[key] = this.map(obj1[key], value2);
-      }
-      for (const key in obj2) {
-        if (this.isFunction(obj2[key]) || diff[key] !== undefined) {
-          continue;
-        }
-
-        diff[key] = this.map(undefined, obj2[key]);
-      }
-
-      return diff;
-    },
-    compareValues: function (value1: any, value2: any) {
-      if (value1 === value2) {
-        return this.VALUE_UNCHANGED;
-      }
-      if (
-        this.isDate(value1) &&
-        this.isDate(value2) &&
-        value1.getTime() === value2.getTime()
-      ) {
-        return this.VALUE_UNCHANGED;
-      }
-      if (value1 === undefined) {
-        return this.VALUE_CREATED;
-      }
-      if (value2 === undefined) {
-        return this.VALUE_DELETED;
-      }
-      return this.VALUE_UPDATED;
-    },
-    isFunction: function (x: any) {
-      return Object.prototype.toString.call(x) === "[object Function]";
-    },
-    isArray: function (x: any) {
-      return Object.prototype.toString.call(x) === "[object Array]";
-    },
-    isDate: function (x: any) {
-      return Object.prototype.toString.call(x) === "[object Date]";
-    },
-    isObject: function (x: any) {
-      return Object.prototype.toString.call(x) === "[object Object]";
-    },
-    isValue: function (x: any) {
-      return !this.isObject(x) && !this.isArray(x);
-    },
-  };
-})();
