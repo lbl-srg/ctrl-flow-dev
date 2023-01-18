27c8c5dd441649cbfe582cdf0ba8e04cc0750fd4
Daren Keck <daren.keck@deptagency.com>
2023-01-17 16:28:14 -0800

    Patch project options, update template.json

--- server/src/parser/parser.ts
+++ server/src/parser/parser.ts
@@ -194,7 +194,6 @@ function assertType(type: string) {
 
 export function getProject() {
   const inputs = createProjectInputs();
-
   return inputs[PROJECT_INSTANCE_PATH];
 }
 
@@ -222,6 +221,27 @@ export function createProjectInputs(): { [key: string]: TemplateInput } {
     elementType: "component_clause",
   };
 
+  const optionPatchPaths = [
+    "Buildings.Templates.Data.AllSystems.ashCliZon",
+    "Buildings.Templates.Data.AllSystems.tit24CliZon",
+  ];
+
+  optionPatchPaths.map((p) => {
+    const option = projectInputs[p];
+    if (option) {
+      const prefix = "Buildings.Templates.Data.AllSystems";
+      // patch enable expression with full path so project evaluation
+      // continues to work
+      const enableExpr = option.enable;
+      const oldOperand = enableExpr.operands[0];
+      if (!oldOperand.startsWith(prefix)) {
+        const newOperand = `${prefix}.${enableExpr.operands[0]}`;
+        enableExpr.operands[0] = newOperand;
+        option.enable = enableExpr;
+        projectInputs[p] = option;
+      }
+    }
+  });
   return { [PROJECT_INSTANCE_PATH]: spoofedDatAllInstance, ...projectInputs };
 }
 
