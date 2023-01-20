7887d5393047e0f7944a26e3b8850c2ddd7ffe7e
darenkeck-dev <94657107+darenkeck-dev@users.noreply.github.com>
2023-01-20 12:30:45 -0800

    Freeze modelica-buildings, modelica-json versions (#289)
    * Target specific commits for modelica-buildings, modelica-json
    
    * Update modelica-json commit hash
    
    Co-authored-by: Daren Keck <daren.keck@deptagency.com>

--- server/bin/install-modelica-dependencies.sh
+++ server/bin/install-modelica-dependencies.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 set -x
 
+MODELICA_BUILDINGS_COMMIT=e70218d39e3f83402caa87100df426d754e56766
+MODELICA_JSON_COMMIT=a46a361c3047c0a2b3d1cfc9bc8b0a4ced16006a
+
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# Clone modelica buildings commit -
 cd ../dependencies
-git clone -b issue1374_templates --single-branch --depth 1 https://github.com/lbl-srg/modelica-buildings.git
-git clone -b issue157_updateSimJsonStructure --single-branch --depth 1 https://github.com/lbl-srg/modelica-json.git
+git clone https://github.com/lbl-srg/modelica-buildings.git
+cd modelica-buildings
+git checkout $MODELICA_BUILDINGS_COMMIT
+cd -
+
+# Clone modelica-json commit
+git clone https://github.com/lbl-srg/modelica-json.git
+cd modelica-json
+git checkout $MODELICA_JSON_COMMIT
+cd -
+
 git clone -b v3.2.3+build.4 --single-branch --depth 1 https://github.com/modelica/ModelicaStandardLibrary.git
 
 # TODO: remove patching
