c53781e4862965bd03e761d78c09905880df836f
AntoineGautier <agautier@solamen.fr>
2023-09-12 12:00:21 +0200

    Fix cd

--- server/bin/install-modelica-dependencies.sh
+++ server/bin/install-modelica-dependencies.sh
@@ -21,7 +21,7 @@ cd -
 
 git clone -b v3.2.3+build.4 --single-branch --depth 1 https://github.com/modelica/ModelicaStandardLibrary.git
 
-cd ../modelica-json
+cd modelica-json
 git apply $parent_path/bin/maven-install.patch
 make install
 make compile
