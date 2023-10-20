dd6f70ec1e7be7c907b090a6b4f85914c12ccfd7
AntoineGautier <agautier@solamen.fr>
2023-10-20 17:39:21 +0200

    Update MBL commit #
    Related to MBL PR #3540.

--- server/bin/install-modelica-dependencies.sh
+++ server/bin/install-modelica-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-MODELICA_BUILDINGS_COMMIT=6036d14d2d3a12376dab886a62a40d3f475f68ab
+MODELICA_BUILDINGS_COMMIT=cdd3fe8f4db0feaf821eb0f61c4d05635d98e957
 MODELICA_JSON_COMMIT=a46a361c3047c0a2b3d1cfc9bc8b0a4ced16006a
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
