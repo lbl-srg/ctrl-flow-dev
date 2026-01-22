a395052c3e744968422580acf749428e0f48474c
AntoineGautier <agautier@solamen.fr>
2026-01-22 17:24:49 +0100

    Update modelica dependencies, remove obsolete patch
    This includes the changes from
    - https://github.com/lbl-srg/modelica-json/pull/295
    - https://github.com/lbl-srg/modelica-json/pull/298

--- server/bin/install-modelica-dependencies.sh
+++ server/bin/install-modelica-dependencies.sh
@@ -2,7 +2,8 @@
 set -x
 
 MODELICA_BUILDINGS_COMMIT=b399379315641da39b231033b0660100fd6489a5
-MODELICA_JSON_COMMIT=71b45ba6af2a7534c9d62abecf79e62240671e38
+MODELICA_STANDARD_TAG=v4.0.0 # This should be driven by 'uses' annotation from Buildings/package.mo
+MODELICA_JSON_COMMIT=b715c09d3092192779e8eccd80c813f08ea1a8e6
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
@@ -19,7 +20,7 @@ cd modelica-json
 git checkout $MODELICA_JSON_COMMIT
 cd -
 
-git clone -b v3.2.3+build.4 --single-branch --depth 1 https://github.com/modelica/ModelicaStandardLibrary.git
+git clone -b $MODELICA_STANDARD_TAG --single-branch --depth 1 https://github.com/modelica/ModelicaStandardLibrary.git
 
 cd modelica-json
 git apply $parent_path/bin/maven-install.patch
