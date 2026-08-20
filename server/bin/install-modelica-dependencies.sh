3b68f85f8d445521ac6b45b6c8e83fe136f3c950
AntoineGautier <agautier@solamen.fr>
2026-08-20 10:14:00 +0200

    Use shallow fetch instead of full clone for dependencies

--- server/bin/install-modelica-dependencies.sh
+++ server/bin/install-modelica-dependencies.sh
@@ -5,23 +5,26 @@ MODELICA_BUILDINGS_COMMIT=b399379315641da39b231033b0660100fd6489a5
 MODELICA_STANDARD_TAG=v4.0.0 # This should be driven by 'uses' annotation from Buildings/package.mo
 MODELICA_JSON_COMMIT=81d90dac01cce41cbb80f7b0171c673dbcd7788a
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-# Clone modelica buildings commit -
+# Shallow-fetch modelica-buildings at a specific commit
 cd ../dependencies
-git clone https://github.com/lbl-srg/modelica-buildings.git
+mkdir modelica-buildings
 cd modelica-buildings
-git checkout $MODELICA_BUILDINGS_COMMIT
+git init -q
+git remote add origin https://github.com/lbl-srg/modelica-buildings.git
+git fetch --depth 1 origin $MODELICA_BUILDINGS_COMMIT
+git checkout -q FETCH_HEAD
 cd -
 
-# Clone modelica-json commit
-git clone https://github.com/lbl-srg/modelica-json.git
+# Shallow-fetch modelica-json at a specific commit
+mkdir modelica-json
 cd modelica-json
-git checkout $MODELICA_JSON_COMMIT
+git init -q
+git remote add origin https://github.com/lbl-srg/modelica-json.git
+git fetch --depth 1 origin $MODELICA_JSON_COMMIT
+git checkout -q FETCH_HEAD
 cd -
 
 git clone -b $MODELICA_STANDARD_TAG --single-branch --depth 1 https://github.com/modelica/ModelicaStandardLibrary.git
 
 cd modelica-json
 make install
-make compile
